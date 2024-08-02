8fc163fae3f02289bc397e360d5a320819c78f11
MViral <160704109+Viral2004@users.noreply.github.com>
2024-08-02 11:35:01 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
-const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/SignUp`, {
-       
-      });
-    
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
-  }
 
-  export default connectDB
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) {
+    console.log("Already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(`mongodb://localhost:27017/SignUp`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    throw new Error("Error connecting to MongoDB");
+  }
+};
+
+export default connectDB;

eb3c49e97c190863b2acb7de74e24bc684accea8
MViral <160704109+Viral2004@users.noreply.github.com>
2024-08-02 11:36:55 +0530

    Update route.js

--- app/api/getImg/route.js
+++ app/api/getImg/route.js
@@ -1,28 +1,29 @@
-
 import connectDB from "@/db/connectDb";
 import Photo from "@/model/Photo";
-
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
-    try {
-        await connectDB();
-        const userEmail = request.headers.get('x-user-email');
-  
-        const photo = await Photo.findOne({email:userEmail}).sort({ createdAt: -1 }); // Get the latest photo
-
-        if (!photo) {
-            return NextResponse.json({ response: "No images found", success: false });
-        }
-
-        return new NextResponse(photo.img.buffer, {
-            headers: {
-                'Content-Type': photo.contentType,
-                'Content-Length': photo.img.length,
-            },
-        });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ response: "Internal Server Error", success: false });
+  try {
+    await connectDB();
+    const userEmail = request.headers.get('x-user-email');
+    if (!userEmail) {
+      return NextResponse.json({ response: "Email header not found", success: false }, { status: 400 });
     }
-}
+    
+    const photo = await Photo.findOne({ email: userEmail }).sort({ createdAt: -1 });
+
+    if (!photo) {
+      return NextResponse.json({ response: "No images found", success: false }, { status: 404 });
+    }
+
+    return new NextResponse(photo.img.buffer, {
+      headers: {
+        'Content-Type': photo.contentType,
+        'Content-Length': photo.img.length,
+      },
+    });
+  } catch (error) {
+    console.error("Internal Server Error", error);
+    return NextResponse.json({ response: "Internal Server Error", success: false }, { status: 500 });
+  }
+};

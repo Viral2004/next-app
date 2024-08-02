185f9e75a38fe4bbac8a773c8b75a85dfc2f6045
MViral <160704109+Viral2004@users.noreply.github.com>
2024-08-02 17:17:37 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,43 +1,37 @@
-// Add "use server" at the beginning of the file to ensure it runs on the server side
 "use server";
 
 import NextAuth from 'next-auth';
-import mongoose from 'mongoose';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import connectDB from '@/db/connectDb';
 import SignUp from '@/model/SignUp';
-import CredentialsProvider from "next-auth/providers/credentials";
-
-const { Schema, model } = mongoose;
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "email" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-                // Add logic here to look up the user from the credentials supplied
-                await connectDB();
-                const user = await SignUp.findOne({ email: credentials.email });
-                
-                if (user && user.password === credentials.password) {
-                    return user;
-                } else {
-                    return null;
-                }
-            }
-        })
-    ],
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            return true;
-        },
-        async session({ session, user, token }) {
-            return session;
-        },
-    }
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        await connectDB();
+        const user = await SignUp.findOne({ email: credentials.email });
+        if (user && user.password === credentials.password) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async session({ session, user, token }) {
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };

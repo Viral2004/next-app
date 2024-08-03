f224c783f1e6759e96c39f800c91e702c9ee0a2a
MViral <160704109+Viral2004@users.noreply.github.com>
2024-08-03 09:49:09 +0530

    Update route.js

--- app/api/getImg/route.js
+++ app/api/getImg/route.js
@@ -2,10 +2,10 @@ import connectDB from "@/db/connectDb";
 import Photo from "@/model/Photo";
 import { NextResponse } from "next/server";
 
-export const GET = async (request) => {
+export async function GET(request) {
   try {
     await connectDB();
-    
+
     const photo = await Photo.findOne().sort({ createdAt: -1 });
 
     if (!photo) {
@@ -16,10 +16,11 @@ export const GET = async (request) => {
       headers: {
         'Content-Type': photo.contentType,
         'Content-Length': photo.img.length,
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
       },
     });
   } catch (error) {
     console.error("Internal Server Error", error);
     return NextResponse.json({ response: "Internal Server Error", success: false }, { status: 500 });
   }
-};
+}
